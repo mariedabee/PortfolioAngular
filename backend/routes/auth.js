3cd6448f4a12fe1e77299adb53416da0b7ff7168
Marie Dabee <mariedabee@maries-air.speedport.ip>
2024-12-02 17:20:57 +0100

    adding docker

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
